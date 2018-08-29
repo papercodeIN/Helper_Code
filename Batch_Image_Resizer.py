506a263546641e76bf7bb693b4501051f6292530
PY_LIN_ <urvishmnakum@gmail.com>
2018-08-29 14:41:45 +0530

    Add files via upload

--- Batch_Image_Resizer.py
+++ Batch_Image_Resizer.py
@@ -1,5 +1,5 @@
 __author__ = 'Nakum Urvish'
-__Version__ = '0.1'
+__Version__ = '0.2'
 """
 Script to resize all image in current working directory.
 And save each image as new file in "Resized_Image" folder.
@@ -9,14 +9,26 @@ import os
 import glob
 import cv2
 
-images = glob.glob('*.jpg')
-print(images)
+File_types = ("*.jpg" , "*.png" , "*.jpeg" , "*.bmp")
 
+# Create Folder to store resized image
 Folder = "Resized_Image"
 if not os.path.exists(Folder):
 	os.mkdir(Folder)
+	print("[+] Resized_Image Folder Created.")
 
-for image in images:
+
+files_grabbed = []
+
+for file in File_types:
+	files_grabbed.extend(glob.glob(file))
+
+
+for image in files_grabbed:
+	print("[+] Resizing Image : " + image)
 	pic = cv2.imread(image, 1)
 	pic = cv2.resize(pic, (277,277))
 	cv2.imwrite(Folder + '/' + image, pic)
+
+print("[+] Resize Complete.")
+	
