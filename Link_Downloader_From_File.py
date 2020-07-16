397b645d9611ea8e28616444f22bc3e14a11408a
PY_LIN_ <urvishmnakum@gmail.com>
2020-07-16 18:43:01 +0530

    Update Link_Downloader_From_File.py
    Small Update

--- Link_Downloader_From_File.py
+++ Link_Downloader_From_File.py
@@ -1,32 +1,32 @@
 import os
-
-# You can use this one too.
 #import wget
-
+#from wget import bar_thermometer
 count = 0
 
 # Path to URL File.
-# text file should only contain URL which can be downloadable.
-file_path = "/folder/file.txt"
+link_path = "/home/parrot/dw.txt"
 
 
 # Count number of URL available in file
-with open(file_path, 'r') as file:
+with open(link_path, 'r') as file:
     for URL in file:
         count += 1
 
 count = int(count)
-print("INFO     [+]File Path:" , file_path)
+print("INFO     [+]File Path:" , link_path)
 
 # Number of URL available in file
 print("INFO     [+]Total Number Of URL in File are:" , int(count))
 
-# Download filr from each URl
-with open(file_path, 'r') as file:
+counter = 0
+# Download file from each URl
+with open(link_path, 'r') as file:
 	for i in range (1,count):
-	    for url in file:
-	    	print("INFO     [+]Downloading...        ")
-	    	os.system('wget -c -q --show-progress %s'%url)
+	    for url in file and path in open(file_path , 'r') :
+	    	counter += 1
+	    	print("\nINFO     [+]Downloading...%s " %counter + "of %r" %count )
+	    	os.system('wget -c -q --show-progress %s' %url)
+#	    	wget.download(url)	    	
 #	    	wget.download(url, bar_thermometer(i, int(count)))
 
-print("INFO     [+]Download Complete.")
+print("\nINFO     [+]Download Complete.")
